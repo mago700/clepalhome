7d684bf27db17dcdbb631d62f4595d9d6e0a1aa0
Celio <celiosatopt@gmail.com>
2022-08-03 17:29:29 -0300

    Email

---
+++ src/main/java/com/clepalhome/vendas/domain/Cliente.java
@@ -0,0 +1,119 @@
+package com.clepalhome.vendas.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Cliente implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String email;
+	
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+	private List<Morada> morada = new ArrayList<>();
+	
+	@ElementCollection
+	@CollectionTable(name = "telefone")
+	private Set<String> telefones = new HashSet<>();
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+
+	public Cliente() {
+		
+	}
+
+	public Cliente(Integer id, String nome, String email) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public List<Morada> getMorada() {
+		return morada;
+	}
+
+	public void setMorada(List<Morada> morada) {
+		this.morada = morada;
+	}
+
+	public Set<String> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(Set<String> telefones) {
+		this.telefones = telefones;
+	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
---
+++ src/main/java/com/clepalhome/vendas/services/PedidoService.java
@@ -0,0 +1,67 @@
+package com.clepalhome.vendas.services;
+
+import java.util.Date;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.clepalhome.vendas.domain.ItemPedido;
+import com.clepalhome.vendas.domain.Pedido;
+import com.clepalhome.vendas.domain.enums.EstadoPagamento;
+import com.clepalhome.vendas.repositories.ItemPedidoRepository;
+import com.clepalhome.vendas.repositories.PagamentoRepository;
+import com.clepalhome.vendas.repositories.PedidoRepository;
+import com.clepalhome.vendas.services.exception.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired 
+	private PedidoRepository repo;
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	@Autowired 
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired 
+	private ProdutoService produtoService;
+	
+	@Autowired 
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired 
+	private EmailService emailService;
+	
+//===================================== LISTAR =============================================
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+//==================================== INSERIR =============================================
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+	
+	obj = repo.save(obj);
+	pagamentoRepository.save(obj.getPagamento());
+		for(ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		emailService.sendOrderConfirmationEmail(obj);
+		return obj;
+	}
+}
